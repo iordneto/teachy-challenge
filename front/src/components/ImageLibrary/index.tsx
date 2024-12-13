86307f8a114dc17106f33bb6ceb214c406b7559d
iord1 <iord@getstic.ky>
2024-12-13 16:00:22 -0300

    feat: add upload button

--- front/src/components/ImageLibrary/index.tsx
+++ front/src/components/ImageLibrary/index.tsx
@@ -1,10 +1,29 @@
+import { useDragAndDrop } from "@/contexts/DragAndDropContext";
+import { useRef } from "react";
 import { IoCloudUpload } from "react-icons/io5";
 import ImageList from "./ImageList";
 import useImageList from "./ImageList/useImageList";
 
 const ImageLibrary = () => {
+  const { onImageUpload } = useDragAndDrop();
   const { images } = useImageList();
   const isListEmpty = images?.length === 0;
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      onImageUpload(file);
+    }
+    e.target.value = ""; // Reseta o input para permitir o upload do mesmo arquivo novamente.
+  };
 
   return (
     <div
@@ -14,8 +33,19 @@ const ImageLibrary = () => {
           : "justify-between items-start"
       }  px-3 py-6 bg-white h-screen overflow-y-auto border-r-[1px] border-solid border-[rgba(57,76,96,.15)]  shadow-md min-w-[320px]`}
     >
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+
       {isListEmpty && (
-        <button className="flex flex-col items-center gap-2 p-4 w-full text-[#0D1216] rounded-lg border-[1px] border-dashed border-[rgba(57,76,96,.15)] max-w-[160px]">
+        <button
+          onClick={handleButtonClick}
+          className="flex flex-col items-center gap-2 p-4 w-full text-[#0D1216] rounded-lg border-[1px] border-dashed border-[rgba(57,76,96,.15)] max-w-[160px]"
+        >
           <IoCloudUpload size={50} />
           Upload Image or Drag and Image
         </button>
@@ -23,7 +53,10 @@ const ImageLibrary = () => {
 
       <ImageList />
       {!isListEmpty && (
-        <button className="flex flex-row  items-center justify-center gap-2 p-4 w-full text-[#0D1216] rounded-lg border-[1px] border-solid border-[rgba(57,76,96,.15)] text-center hover:bg-slate-100 ">
+        <button
+          onClick={handleButtonClick}
+          className="flex flex-row  items-center justify-center gap-2 p-4 w-full text-[#0D1216] rounded-lg border-[1px] border-solid border-[rgba(57,76,96,.15)] text-center hover:bg-slate-100 "
+        >
           <IoCloudUpload size={25} />
           Upload Image
         </button>
