3518398dc47caf059fe8f66db69746ef39b08a9e
iord1 <iord@getstic.ky>
2024-12-13 09:28:35 -0300

    feat: add loading state and fix dragging

--- front/src/components/ImageLibrary/ImageList/ImageItem/index.tsx
+++ front/src/components/ImageLibrary/ImageList/ImageItem/index.tsx
@@ -1,15 +1,16 @@
 import { UserImage } from "@/lib/dexieClient";
+import Image from "next/image";
 import React from "react";
 import useImageItem from "./useImageItem";
 
 const areEqual = (
-  prevProps: { image: UserImage },
-  nextProps: { image: UserImage }
+  prevProps: { image?: UserImage },
+  nextProps: { image?: UserImage }
 ) => {
-  return prevProps.image.id === nextProps.image.id;
+  return prevProps.image?.id === nextProps.image?.id;
 };
 
-const ImageItem = React.memo(({ image }: { image: UserImage }) => {
+const ImageItem = React.memo(({ image }: { image?: UserImage }) => {
   const {
     getImageUrlFromFileData,
     handleDragStart,
@@ -17,17 +18,32 @@ const ImageItem = React.memo(({ image }: { image: UserImage }) => {
     isDragging,
   } = useImageItem();
 
+  const backgroundImage: string | null = image?.fileData
+    ? getImageUrlFromFileData(image.fileData)
+    : null;
+
+  const imageTitle = image?.fileName || "Imagem sendo carregada";
+
   return (
     <div
-      className={`w-[100px] h-[100px] rounded-[8px] bg-transparent bg-cover bg-center cursor-pointer`}
-      style={{
-        backgroundImage: `url(${getImageUrlFromFileData(image.fileData)})`,
-        cursor: isDragging ? "grabbing" : "grab",
-      }}
+      className={`w-[100px] h-[100px] rounded-[8px] bg-transparent bg-cover bg-center cursor-pointer overflow-hidden
+        ${!backgroundImage && "bg-slate-200"}
+        ${isDragging ? "cursor-grabbing" : "cursor-grab"} `}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
       draggable
-    ></div>
+    >
+      {backgroundImage && (
+        <Image
+          src={backgroundImage}
+          alt={imageTitle}
+          className={`w-full h-full object-cover pointer-events-none`}
+          draggable={false}
+          width={100}
+          height={100}
+        />
+      )}
+    </div>
   );
 }, areEqual);
 
